20cd2b2ff9347e4738e3e02c1815dd03ce588f92
Leon Hoos <leon.hoos@conciso.de>
2022-06-04 16:57:21 +0200

    Add constructors to ChatMessageToTelegram

--- src/main/java/de/Linus122/TelegramComponents/ChatMessageToTelegram.java
+++ src/main/java/de/Linus122/TelegramComponents/ChatMessageToTelegram.java
@@ -2,6 +2,16 @@ package de.Linus122.TelegramComponents;
 
 public class ChatMessageToTelegram extends Cancellable {
 	public String text;
+
+	public ChatMessageToTelegram() {
+	}
+
+	public ChatMessageToTelegram(String text, long chat_id, String parse_mode) {
+		this.text = text;
+		this.chat_id = chat_id;
+		this.parse_mode = parse_mode;
+	}
+
 	public long chat_id;
 	public String parse_mode;
 }
