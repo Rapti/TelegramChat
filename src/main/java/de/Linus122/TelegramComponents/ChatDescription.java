8b0f8744f4c7f62c91925a897e353c5e1fde181d
Leon Hoos <leon@die-hoosen.de>
2022-06-18 11:12:53 +0200

    Comment out parse_mode
    It doesn't work anyway right now because Telegram doesn't support it.

--- src/main/java/de/Linus122/TelegramComponents/ChatDescription.java
+++ src/main/java/de/Linus122/TelegramComponents/ChatDescription.java
@@ -4,9 +4,10 @@ public class ChatDescription {
 	public long chat_id;
 	public String description;
 
-	// parse_mode doesn't seem to be supported by setChatDescription right now ...
+	// Chat Descriptions only support plain text right now ...
 	// I'll leave it here in case support gets added in the future.
 	// Go ahead and nag the Telegram devs if you want this feature to work!
 	// https://core.telegram.org/bots/api#setchatdescription
-	public String parse_mode = "Markdown";
+
+	//public String parse_mode = "Markdown";
 }
